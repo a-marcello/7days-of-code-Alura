32240ae453d5170f98b01c7760767dc54b7532ce
a-marcello <90654906+a-marcello@users.noreply.github.com>
2022-11-01 10:37:30 -0300

    Create Challenge-1.js
    first challenge

---
+++ Challenge-1.js
@@ -0,0 +1,55 @@
+let numeroUm = 2
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if ( numeroUm == stringUm) {
+    if(typeof(numeroUm)==typeof(stringUm)){
+        console.log('As variáveis numeroUm e stringUm tem o mesmo valor e mesmo tipo')
+    }
+    else{
+        console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e tipos diferentes')
+    }
+} else {
+    if(typeof(numeroUm)==typeof(stringUm)){
+        console.log('As variáveis numeroUm e stringUm não tem o mesmo valor e tem mesmo tipo')
+    }
+    else{
+        console.log('As variáveis numeroUm e stringUm não tem o mesmo valor e tem  tipos diferentes')
+    }
+}
+
+if ( numeroTrinta == stringTrinta) {
+    if(typeof(numeroTrinta)==typeof(stringTrinta)){
+        console.log('As variáveis numeroUm e stringUm tem o mesmo valor e mesmo tipo')
+    }
+    else{
+        console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e tipos diferentes')
+    }
+} else {
+    if(typeof(numeroTrinta)==typeof(stringTrinta)){
+        console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo valor e tem mesmo tipo')
+    }
+    else{
+        console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo valor e  tipos diferentes')
+    }
+}
+
+if ( numeroDez == stringDez) {
+    if(typeof(numeroDez)==typeof(stringDez)){
+        console.log('As variáveis numeroDez e stringDez tem o mesmo valor e mesmo tipo')
+    }
+    else{
+        console.log('As variáveis numeroDez e stringDez tem o mesmo valor e tipos diferentes')
+    }
+} else {
+    if(typeof(numeroDez)==typeof(stringDez)){
+        console.log('As variáveis numeroDez e stringDez não tem o mesmo valor e tem mesmo tipo')
+    }
+    else{
+        console.log('As variáveis numeroDez e stringDez não tem o mesmo valor e  tem tipos diferentes')
+    }
+}
+
